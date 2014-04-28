6b87b263b68cd6c242af03ae5c72268c1b224521
Gautam <gautam2004@gmail.com>
2014-04-28 00:15:42 -0400

    initial project version

---
+++ color_hist.m
@@ -0,0 +1,13 @@
+function [ desc ] = color_hist( im, mask, numBins, varargin )
+%COLOR_HIST Summary of this function goes here
+%   Detailed explanation goes here
+
+desc = [];
+% numBins = 8;
+binSize = 256/numBins;
+binCenters = (binSize-1)/2:binSize:255;
+for c = 1:3
+   r = im(:,:,c);
+   % keyboard
+   desc = [desc hist(double(r(mask>0)),binCenters)/sum(mask(:)>0)];
+end
