d53bc3b5989fe07df4f59b431033ac2ceb3b72f0
Gautam <gautam2004@gmail.com>
2018-05-22 23:46:39 -0500

    Update color_hist.m

--- color_hist.m
+++ color_hist.m
@@ -7,6 +7,5 @@ binSize = 256/numBins;
 binCenters = (binSize-1)/2:binSize:255;
 for c = 1:3
    r = im(:,:,c);
-   % keyboard
    desc = [desc hist(double(r(mask>0)),binCenters)/sum(mask(:)>0)];
 end
